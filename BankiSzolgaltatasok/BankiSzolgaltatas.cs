5a1b7f28edab12337e450cc089fe6517b3893757
Petrik Diak <diak@petrik.hu>
2023-10-19 08:29:06 +0200

    Tulajdonos sealed class létrehozása BankiSzolgaltatas abstract class létrehozása

---
+++ BankiSzolgaltatasok/BankiSzolgaltatas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+	internal abstract class BankiSzolgaltatas
+	{
+		Tulajdonos tulajdonos;
+
+		protected BankiSzolgaltatas(Tulajdonos tulajdonos)
+		{
+			this.tulajdonos = tulajdonos;
+		}
+
+		internal Tulajdonos Tulajdonos { get => tulajdonos; }
+	}
+}
