326ea87ac20c4ae66a761835cab85eae2e9d7c51
Petrik Diak <diak@petrik.hu>
2023-10-19 08:42:10 +0200

    Számla abstract class létrehozása

---
+++ BankiSzolgaltatasok/Szamla.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+	internal abstract class Szamla : BankiSzolgaltatas
+	{
+		protected int aktualisEgyenleg;
+
+		public Szamla(Tulajdonos tulajdonos) : base(tulajdonos)
+		{
+			
+		}
+
+		public int AktualisEgyenleg { get => aktualisEgyenleg; }
+
+		public void Befizet(int osszeg)
+		{
+			osszeg += aktualisEgyenleg;
+		}
+
+		public abstract bool Kivesz(int osszeg);
+	}
+}
